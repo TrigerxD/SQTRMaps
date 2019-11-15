877436bb115154612729af4655377eb457aac81f
Grzegorz863 <gn22@o2.pl>
2019-11-15 14:14:50 +0100

    config change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ const position = [50.288706, 18.675072]
 
 function App() {
   return (
-      <Map center={position} zoom={17}>
+      <Map center={position} zoom={17} maxZoom={19}>
         <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
