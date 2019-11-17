4416f3c078195557e1a84b733bc62c4a7bfbcffe
Kamil Ismail <ismail.katowice@gmail.com>
2019-11-17 18:15:45 +0100

    added geolocalization and buttons
    If geolocalization is enabled in browser map shows user location, if not map shows location of AEiI building. Added buttons: find me, send localization, log in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,92 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import {Map, Marker, Popup, TileLayer} from 'react-leaflet'
+import Control from 'react-leaflet-control';
 
-const position = [50.288706, 18.675072]
+const aeiLat = 50.28868461815858
+const aeiLng = 18.67756247520447
 
-function App() {
-  return (
-      <Map center={position} zoom={17} maxZoom={19}>
-        <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-        />
-        <Marker position={position}>
-          <Popup>AEiI</Popup>
-        </Marker>
-      </Map>
-  );
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            latitude: aeiLat,
+            longitude: aeiLng,
+        }
+        this.getMyLocation = this.getMyLocation.bind(this)
+        this.newMarkerPosition = this.newMarkerPosition.bind(this);
+        this.sendLocalization = this.sendLocalization.bind(this);
+        this.login = this.login.bind(this);
+    }
+
+    componentDidMount() {
+        this.getMyLocation()
+    }
+
+    newMarkerPosition(e) {
+        const {lat, lng} = e.latlng;
+        this.setState({
+            latitude: lat,
+            longitude: lng,
+        });
+    }
+
+    sendLocalization() {
+        console.log('Sending loc to server: ' + this.state.latitude + ' ' + this.state.longitude);
+    }
+
+    login() {
+        console.log('Log in');
+    }
+
+    getMyLocation() {
+        const location = window.navigator && window.navigator.geolocation
+        if (location) {
+            location.getCurrentPosition((position) => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                })
+            }, (error) => {
+                this.setState({latitude: aeiLat, longitude: aeiLng})
+            })
+        }
+
+    }
+
+    render() {
+        const {latitude, longitude} = this.state
+        const geoLocation = [latitude, longitude]
+        return (
+            <Map center={geoLocation} zoom={17} maxZoom={19} onClick={this.newMarkerPosition}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                />
+
+                <Control position="topright">
+                    <button onClick={this.sendLocalization}>
+                        Wyślij lokalizację
+                    </button>
+                </Control>
+
+                <Control position="topright">
+                    <button onClick={this.getMyLocation}>
+                        Wybierz moją lokalizację
+                    </button>
+                </Control>
+
+                <Control position="topright">
+                    <button onClick={this.login}>
+                        Zaloguj się
+                    </button>
+                </Control>
+
+                <Marker position={geoLocation}>
+                    <Popup>Lokalizacja hulajnogi</Popup>
+                </Marker>
+            </Map>
+        );
+    }
 }
 
 export default App;
